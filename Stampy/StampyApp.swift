5dea869dc506c8dd1b6aa96c288f860e0313a365
urassh <shuto.1011@icloud.com>
2024-11-01 23:14:29 +0900

    Initial Commit

---
+++ Stampy/StampyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StampyApp.swift
+//  Stampy
+//
+//  Created by 浦山秀斗 on 2024/11/01.
+//
+
+import SwiftUI
+
+@main
+struct StampyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
