3d0f45dd09f843011ec2b3026ec250702fe6e6d2
urassh <shuto.1011@icloud.com>
2024-11-03 12:33:04 +0900

    feat: MapViewのUIを簡単に実装

---
+++ Stampy/View/Map/MapView.swift
@@ -0,0 +1,32 @@
+//
+//  MapView.swift
+//  Stampy
+//
+//  Created by 浦山秀斗 on 2024/11/03.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        ZStack {
+            Map()
+            VStack {
+                Spacer()
+                
+                ScrollView(.horizontal) {
+                    HStack(spacing: 12) {
+                        UserCardView()
+                        UserCardView()
+                        UserCardView()
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MapView()
+}
---
+++ Stampy/View/UtilView/CheckBoxToggleStyle.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Stampy
+//
+//  Created by 浦山秀斗 on 2024/11/03.
+//
+
