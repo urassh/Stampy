cb9fcae6f08f33f9d0eceea56ec9d3e89720724b
urassh <shuto.1011@icloud.com>
2024-11-03 12:34:41 +0900

    feat: ToggleStyleを導入

--- Stampy/View/UtilView/CheckBoxToggleStyle.swift
+++ Stampy/View/UtilView/CheckBoxToggleStyle.swift
@@ -5,3 +5,39 @@
 //  Created by 浦山秀斗 on 2024/11/03.
 //
 
+import SwiftUI
+
+struct CheckBoxToggleStyle:ToggleStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        Button{
+            configuration.isOn.toggle()
+        } label: {
+            HStack{
+                Image(systemName: configuration.isOn
+                      ? "checkmark.circle.fill"
+                      : "circle")
+                configuration.label
+            }
+        }
+    }
+}
+
+struct CheckBoxConst: View {
+    let label: String
+    let isOn: Bool
+    
+    var body: some View {
+        HStack{
+            VStack {
+                Image(systemName: isOn
+                      ? "checkmark.circle.fill"
+                      : "circle")
+                Spacer()
+            }
+            
+            Text(label)
+                .lineLimit(nil)
+                .fixedSize(horizontal: false, vertical: true)
+        }
+    }
+}
