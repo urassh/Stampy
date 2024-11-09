909d162beb895bded5b59985c63f72afeeb5579f
urassh <shuto.1011@icloud.com>
2024-11-09 14:27:29 +0900

    fix: rename UserDummyGateway

--- Stampy/Infrastructure/UserDummyGateway.swift
+++ Stampy/Infrastructure/UserDummyGateway.swift
@@ -5,7 +5,7 @@
 //  Created by 浦山秀斗 on 2024/11/09.
 //
 
-class UserLocalGateway: UserGatewayProtocol {
+class UserDummyGateway: UserGatewayProtocol {
     private var users: [UserRecord] = [
         UserRecord(uid: "1", name: "urassh", goal_id: "1"),
         UserRecord(uid: "2", name: "atushi", goal_id: "2"),
