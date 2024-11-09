a22560b3a9b27c8a7b39c06e304c191df8e9528d
urassh <shuto.1011@icloud.com>
2024-11-09 18:31:31 +0900

    feat: 静的なdummyDataを定義

--- Stampy/Infrastructure/Dummies/GoalDummyGateway.swift
+++ Stampy/Infrastructure/Dummies/GoalDummyGateway.swift
@@ -9,8 +9,16 @@ import Foundation
 
 class GoalDummyGateway: GoalGatewayProtocol {
     private var goals: [GoalRecord] = [
-            GoalRecord(id: "1", user_id: "1", title: "Ruby on Rails を頑張る!!", createdAt: Date().addingTimeInterval(-3 * 24 * 60 * 60)), // 3日前
-            GoalRecord(id: "2", user_id: "2", title: "Swiftを頑張る!!", createdAt: Date().addingTimeInterval(-10 * 24 * 60 * 60)) // 10日前
+            GoalRecord(
+                id: "12345678-1234-1234-1234-1234567890AB",
+                user_id: "12345678-1234-1234-1234-1234567890AB",
+                title: "Ruby on Rails を頑張る!!",
+                createdAt: Date().addingTimeInterval(-3 * 24 * 60 * 60)), // 3日前
+            GoalRecord(
+                id: "87654321-4321-4321-4321-BA0987654321",
+                user_id: "87654321-4321-4321-4321-BA0987654321",
+                title: "Swiftを頑張る!!",
+                createdAt: Date().addingTimeInterval(-10 * 24 * 60 * 60)) // 10日前
         ]
     
     func fetchFromUser(user_id: String) async -> [GoalRecord] {
--- Stampy/Infrastructure/Dummies/UserDummyGateway.swift
+++ Stampy/Infrastructure/Dummies/UserDummyGateway.swift
@@ -7,8 +7,8 @@
 
 class UserDummyGateway: UserGatewayProtocol {
     private var users: [UserRecord] = [
-        UserRecord(uid: "1", name: "urassh"),
-        UserRecord(uid: "2", name: "atushi"),
+        UserRecord(uid: "12345678-1234-1234-1234-1234567890AB", name: "urassh", goal_id: "1"),
+        UserRecord(uid: "87654321-4321-4321-4321-BA0987654321", name: "atushi", goal_id: "2"),
     ]
     
     func fetch(id: String) async -> UserRecord? {
