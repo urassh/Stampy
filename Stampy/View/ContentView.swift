7e8eb9dfb1d8e89ee0874da60120c14c4a4e3283
urassh <shuto.1011@icloud.com>
2024-11-09 18:33:59 +0900

    feat: loginuserも静的なUUIDに変更

--- Stampy/View/ContentView.swift
+++ Stampy/View/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 class LoginUser {
-    static let shared: AppUser = .init(id: "1", name: "urassh")
+    static let shared: AppUser = .init(id: "12345678-1234-1234-1234-1234567890AB", name: "urassh")
     
     private init() {}
 }
