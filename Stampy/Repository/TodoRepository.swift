ff3b8e71184ed0405eefc67a7666ee872b5ca0cc
urassh <shuto.1011@icloud.com>
2024-11-10 22:51:52 +0900

    feat: todo repository

--- Stampy/Repository/TodoRepository.swift
+++ Stampy/Repository/TodoRepository.swift
@@ -5,3 +5,14 @@
 //  Created by 浦山秀斗 on 2024/11/10.
 //
 
+class TodoRepository : TodoRepositoryProtocol {
+    private let todoGateway: TodoGatewayProtocol
+    
+    init(todoGateway: TodoGatewayProtocol) {
+        self.todoGateway = todoGateway
+    }
+    
+    func execute(from goal: Goal) async -> [Todo] {
+        todoGateway.fetchTodos(goal_id: goal.id.uuidString)
+    }
+}
