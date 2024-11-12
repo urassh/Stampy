568d55aba6abf43c6e6b04915f283a3e9bf25c1d
urassh <shuto.1011@icloud.com>
2024-11-12 22:18:19 +0900

    feat: MapUserをidentifiableに準拠させた

--- Stampy/Domain/MapUser.swift
+++ Stampy/Domain/MapUser.swift
@@ -5,7 +5,10 @@
 //  Created by 浦山秀斗 on 2024/11/12.
 //
 
-struct MapUser {
+struct MapUser : Identifiable {
+    let id: UUID {
+        user.id
+    }
     let user: AppUser
     let goal: Goal
     let todo: [Todo]
