0231d97a239bbc785c2bbf888da939a69bdbcb57
urassh <shuto.1011@icloud.com>
2024-11-14 20:28:26 +0900

    feat: GetTextMessageUsecase

---
+++ Stampy/Usecase/Message/GetTextMessageUsecase.swift
@@ -0,0 +1,15 @@
+//
+//  GetMessageUsecase.swift
+//  Stampy
+//
+//  Created by 浦山秀斗 on 2024/11/14.
+//
+
+class GetTextMessageUsecase {
+    private let goalMessageRepository: GoalMessageRepositoryProtocol = GoalMessageRepository()
+    
+    func execute(goal: Goal) async -> [TextMessage] {
+        let fetchedMessages = await goalMessageRepository.getGoalMessages(goal: goal)
+        return fetchedMessages.compactMap { $0 as? TextMessage }
+    }
+}
