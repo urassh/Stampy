34eeaded37ea2eeea61d881fae361d61d9902669
urassh <shuto.1011@icloud.com>
2024-11-14 20:16:17 +0900

    feat: goalMessagesのダミーデータを追加

--- Stampy/Infrastructure/Dummies/GoalMessageDummyGateway.swift
+++ Stampy/Infrastructure/Dummies/GoalMessageDummyGateway.swift
@@ -6,7 +6,20 @@
 //
 
 class GoalMessageDummyGateway: GoalMessageGatewayProtocol {
-    private static var goalMessages: [GoalMessageRecord] = []
+    private static var goalMessages: [GoalMessageRecord] = [
+        GoalMessageRecord(
+            id: "12345678-1234-1234-1234-1234567890AB",
+            sender_id: "87654321-4321-4321-4321-BA0987654321",
+            goal_id: "12345678-1234-1234-1234-1234567890AB",
+            content: "Rails頑張ってね!!",
+            type: "text"),
+        GoalMessageRecord(
+            id: "87654321-4321-4321-4321-BA0987654321",
+            sender_id: "87654321-4321-4321-4321-BA0987654321",
+            goal_id: "12345678-1234-1234-1234-1234567890AB",
+            content: "good",
+            type: "stamp"),
+    ]
     
     func getGoalMessages(goalId: String) async -> [GoalMessageRecord] {
         Self.goalMessages.filter { $0.goal_id == goalId }
