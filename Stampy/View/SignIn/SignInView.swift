564461b48838bcc754e71e6d50644308935dc00d
Atsushi Hosaka <hosaka2917@gmail.com>
2024-11-16 02:20:52 +0900

    画面大体できた

--- Stampy/View/SignIn/SignInView.swift
+++ Stampy/View/SignIn/SignInView.swift
@@ -11,29 +11,39 @@ struct SignInView: View {
     @ObservedObject var viewmodel: SignInViewModel = .init()
     
     var body: some View {
-        VStack {
-            Text("Stampy")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            
-            CustomTextField(initialText: "", placeholder: "email", delegate: viewmodel.emailCoordinator)
-            CustomTextField(initialText: "", placeholder: "password", delegate: viewmodel.passwordCoordinator)
-                .padding(.bottom, 20)
-            
-            if !viewmodel.errorMessage.isEmpty {
-                Text(viewmodel.errorMessage)
-                    .foregroundStyle(.red)
+        NavigationStack {
+            VStack {
+                Text("Stampy")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                CustomTextField(initialText: "", placeholder: "email", delegate: viewmodel.emailCoordinator)
+                CustomTextField(initialText: "", placeholder: "password", delegate: viewmodel.passwordCoordinator)
+                    .padding(.bottom, 20)
+                
+                if !viewmodel.errorMessage.isEmpty {
+                    Text(viewmodel.errorMessage)
+                        .foregroundStyle(.red)
+                }
+                
+                Button {
+                    viewmodel.signIn()
+                } label: {
+                    Text("SignIn")
+                        .foregroundStyle(.white)
+                }
+                .padding()
+                .background(.blue.opacity(0.8))
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                
+                NavigationLink {
+                    SignUpView()
+                } label: {
+                    Text("アカウント作成")
+                        .foregroundStyle(.secondary)
+                }
+                .padding(.top, 30)
             }
-            
-            Button {
-                viewmodel.signIn()
-            } label: {
-                Text("SignIn")
-                    .foregroundStyle(.white)
-            }
-            .padding()
-            .background(.blue.opacity(0.8))
-            .clipShape(RoundedRectangle(cornerRadius: 12))
         }
     }
 }
